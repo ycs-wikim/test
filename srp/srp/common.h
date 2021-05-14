de2c63309d2c2030f3da16da4ca032879998c7cd
wikim <unangel@yuhan.ac.kr>
2021-05-14 14:50:11 +0900

    [ + ] 내용 추가 [ - ] 내용 삭제 [ ! ] 주의 사항
    Project Setting

---
+++ srp/srp/common.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#define GAME_STR_DRAW	"÷ס±ט"
+#define GAME_STR_WIN	"ÀÌ±ט"
+#define GAME_STR_LOSE	"Áü"
+
+#define GAME_DRAW		0
+#define GAME_USER_LOSE	1
+#define GAME_USER_WIN	2
+
+static const int g_gameMatrix[3][3] = {
+	{
+		GAME_DRAW, GAME_USER_LOSE, GAME_USER_WIN
+		},
+	{
+		GAME_USER_WIN, GAME_DRAW, GAME_USER_LOSE
+		},
+	{
+		GAME_USER_LOSE, GAME_USER_WIN, GAME_DRAW
+		},
+};
---
+++ srp/srp/interface.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "common.h"
+
+/// game.cpp
+void startGame();
+
+/// random.cpp
+int userInput();
+
+int computValue();
+
+/// utility.cpp
+int ramdomValue();
+
+const char* gameResult(int result);
